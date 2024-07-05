184aa37d915d60f7f4e57fb1f80b29439ceed681
CrypticCortex <kalyanguru18@gmail.com>
2024-07-05 23:43:44 +0530

    chore: Update Introduction component
    Refactor the Introduction component to improve readability and remove unnecessary comments and code.

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -14,19 +14,19 @@ export default function Introduction() {
             <h2 className="text-2xl font-semibold text-lightBlue">AI Researcher & Developer</h2>
             <p className="text-textSecondary text-lg">
               I’m a 4th-year undergraduate at Amrita University with a keen interest in computer vision and visual learning models.
-              I enjoy turning ideas into applications using Flutter and am actively involved in research, particularly focusing on VLMS and CV.
+              I enjoy tinkering around ideas and push myself to build applications using Flutter, totally not a waste of time and am actively involved in research, particularly focusing on VLMs (not entierly LLMs) and Vision tasks.
             </p>
             <p className="text-textSecondary text-lg">
-              I believe in the power of AI to transform industries and improve lives. My projects often explore the intersection of
-              machine learning, computer vision, and natural language processing. Collaborating with fellow researchers and industry professionals,
-              I strive to push the boundaries of whats possible in AI.
+              I believe in the power of AI to transform industries and improve lives. But now-a-days getting fed-up with the current space of AI hype, I mean imagine seeing updates only about LLMs everywhere.
+              I also like to watch random videos of Karpathy, because why not ¯\_(ツ)_/¯.
             </p>
           </div>
           <div className="flex flex-col items-center justify-center mt-8 md:mt-0">
             <div className="profile-circle">
               <Image
                 src="/Profile.png"
-                alt="Portrait"
+                alt="Profile Image"
+                title="Thank you, ifykuk 🌝"
                 width={256}
                 height={256}
                 className="profile-image"
